6985c5de192a4cabdfc37a78b7d138827e39d7e8
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-03-03 18:07:33 +0000

    wordpress getting secrets

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -14,9 +14,10 @@ cd /var/www/html/wordpress
 # Decrypt secrets.enc
 if [ -f /run/secrets/secrets.enc ]; then
     echo "Decrypting secrets file into /run/secrets/secrets.txt..."
-    openssl enc -aes-256-cbc -d -pbkdf2 \
+    openssl enc -aes-256-cbc -d -pbkdf2 -iter 100000 \
         -in $ENCRYPTED_FILE -out $DENCRYPTED_FILE \
         -pass pass:$(cat /run/secrets/secret_key)
+    echo "Decrypted secrets file into /run/secrets/secrets.txt"
 else
     echo "Error: Secrets file not found"
     exit 1
