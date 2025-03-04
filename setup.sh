c37f1df60fc09b3a1e27d4d7aee7052885bf1d12
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-03-04 21:33:23 +0000

    added folders

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ ENV_FILE="$SECRETS_DIR/.env"
 
 # Create necessary directories
 mkdir -p "$VAULT_DIR" "$TLS_DIR"
-mkdir -p ~/data/ws ~/data/wp ~/data/db
+mkdir -p ~/data/ws ~/data/wp ~/data/db ~/data/unrealircd ~/data/doom
 
 # Generate a dummy decryption key if it does not exist
 if [[ ! -f "$KEY_FILE" ]]; then
